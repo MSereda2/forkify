2a321102369d3b84d81d52d82a1ffaf3b1de3a43
Misha <sereda2510@mail.ru>
2019-12-24 19:37:13 +0300

    add module ES6

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,1 @@
 // Global app controller
-import x from './test';
-const num = 23;
-const test = new Map();
-test.set(1, `work`)
-console.log(`nowww ${x} from another module`);
-console.log(test)
-console.log(num)
-
